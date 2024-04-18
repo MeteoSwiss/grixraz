b7de2e2f9e6cfc85e1eb8ecbb076bd964064e9c6
Christian Kanesan <christian.kanesan@meteoswiss.ch>
2024-04-18 15:27:27 +0200

    fix tar

--- install_micromamba.sh
+++ install_micromamba.sh
@@ -2,6 +2,6 @@
 
 prefix=$HOME/.local/bin
 
-mkdir -p ${prefix} && wget -qO- https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvjf - -C ${prefix}/micromamba
+mkdir -p ${prefix} && wget -qO- https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvjf - -C ${prefix} bin/micromamba --strip-components 1
 ${prefix}/micromamba shell init -s bash -p $SCRATCH/micromamba
 source ~/.bashrc
