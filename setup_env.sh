b0b39b82041599f9ef00b4c6e975988c0f43529e
Peter Kroiss <pkroiss@tsa-ln003.cm.cluster>
2024-04-19 13:31:41 +0200

    Switch to https because of permissions

--- setup_env.sh
+++ setup_env.sh
@@ -4,5 +4,5 @@ path=$HOME/eccodes-cosmo-resources
 
 if [ ! -d ${path} ]; then
     # branch that implements the mars model needed for FDB
-    git clone -b revise_mars_model git@github.com:cosunae/eccodes-cosmo-resources.git ${path}
+    git clone -b revise_mars_model https://github.com/cosunae/eccodes-cosmo-resources.git ${path}
 fi
